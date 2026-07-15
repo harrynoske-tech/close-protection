122071404909c184b3da6d827e04d9a87dae2782
harrynoske-tech <harry.noske@gmail.com>
2026-07-15 19:53:31 +1000

    Create enemy.js

---
+++ enemy.js
@@ -0,0 +1,39 @@
+class Enemy {
+
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.speed = 1.6;
+        this.size = 16;
+    }
+
+    update(target) {
+
+        const dx = target.x - this.x;
+        const dy = target.y - this.y;
+
+        const dist = Math.hypot(dx, dy);
+
+        if (dist > 1) {
+
+            this.x += dx / dist * this.speed;
+            this.y += dy / dist * this.speed;
+
+        }
+
+    }
+
+    draw(ctx, cameraX) {
+
+        ctx.fillStyle = "red";
+
+        ctx.fillRect(
+            this.x - cameraX - this.size / 2,
+            this.y - this.size / 2,
+            this.size,
+            this.size
+        );
+
+    }
+
+}
