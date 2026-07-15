6e7954c2bfaf114b84cd100caabe9c5bfed55109
harrynoske-tech <harry.noske@gmail.com>
2026-07-15 19:29:30 +1000

    Update player.js

--- js/player.js
+++ js/player.js
@@ -1,1 +1,18 @@
+class Player {
+    constructor(x, y, colour) {
+        this.x = x;
+        this.y = y;
+        this.colour = colour;
+        this.size = 16;
+    }
 
+    draw(ctx, cameraX) {
+        ctx.fillStyle = this.colour;
+        ctx.fillRect(
+            this.x - cameraX - this.size / 2,
+            this.y - this.size / 2,
+            this.size,
+            this.size
+        );
+    }
+}
