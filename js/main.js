436d2ee1747f3a781f6fd773078013b30cbfded4
harrynoske-tech <harry.noske@gmail.com>
2026-07-16 19:52:38 +1000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -214,8 +214,8 @@ function updateMovement() {
             const nx = dx / dist;
             const ny = dy / dist;
 
-            guard.x += nx * 3;
-            guard.y += ny * 3;
+        guard.x += nx * guard.speed;
+        guard.y += ny * guard.speed;
 
         }
 
