4e8d0ac6cca08641ba187d3a9a3d81044f8a227d
harrynoske-tech <harry.noske@gmail.com>
2026-07-17 06:45:17 +1000

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,1 +1,19 @@
+// --------------------------------------------------
+// GAME STATE
+// --------------------------------------------------
 
+let score = 0;
+let cash = 0;
+let wave = 1;
+
+let enemiesRemaining = 3;
+
+let waveCountdown = 180;
+
+let betweenWaves = false;
+let shopOpen = false;
+
+const bullets = [];
+const enemies = [];
+
+const MAX_BULLETS = 40;
